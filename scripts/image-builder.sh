39055843e7b29869ab1ef67e8c1d623c1026e697
Piotr Rzepkowski <piotr.rzepkowski98@gmail.com>
2023-05-06 12:54:59 +0200

    Helm: Display docker tag on Application's mani page
    Add docker tag env variable to the application, which
    is used to display the app version on the main page

--- scripts/image-builder.sh
+++ scripts/image-builder.sh
@@ -10,7 +10,10 @@ DOCKERFILE_PATH=app/Dockerfile
 DOCKERFILE_CONTEXT=.
 
 echo "Building docker image $DOCKER_IMAGE_NAME:$1."
-docker build -t $DOCKER_IMAGE_NAME:$1 -f $DOCKERFILE_PATH $DOCKERFILE_CONTEXT
+docker build \
+    -t $DOCKER_IMAGE_NAME:$1 \
+    --build-arg DOCKER_TAG=$1 \
+    -f $DOCKERFILE_PATH $DOCKERFILE_CONTEXT
 
 echo "Loading $DOCKER_IMAGE_NAME:$1 into kind cluster."
 kind load docker-image $DOCKER_IMAGE_NAME:$1
